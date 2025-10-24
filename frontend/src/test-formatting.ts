2152d627c5a39bc1cc85fa0cf8a754d9067f3813
Erwin R. Carrasquilla <Rommel.carrasquilla@gmail.com>
2025-10-24 19:10:44 -0400

    chore: Remove test formatting file

--- frontend/src/test-formatting.ts
+++
@@ -1,6 +0,0 @@
-// Test file with intentionally poor formatting
-const testObject = { a: 1, b: 2, c: 3 };
-if (testObject.a) {
-    console.log('This needs formatting');
-}
-export default testObject;
