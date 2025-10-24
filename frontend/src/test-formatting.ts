c5e6a8077bfe878020525d2eff5c9cb0e9b073eb
Erwin R. Carrasquilla <Rommel.carrasquilla@gmail.com>
2025-10-24 18:57:21 -0400

    test: Add test file to demonstrate pre-commit hook formatting

---
+++ frontend/src/test-formatting.ts
@@ -0,0 +1,6 @@
+// Test file with intentionally poor formatting
+const testObject = { a: 1, b: 2, c: 3 };
+if (testObject.a) {
+    console.log('This needs formatting');
+}
+export default testObject;
